7173ae8fe20a4c606002bc44b681c222b044ef07
karthikn9480 <71779380+karthikn9480@users.noreply.github.com>
2025-02-28 18:38:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Deployed Nodejs Application using Github Actions Successfully!');
 });
 
 app.listen(port, () => {
